2ef9d2f62e733b12054fd86066683bbcdedf0017
Anindya Kundu <anindya.k22@outlook.com>
2019-11-04 20:47:48 +0530

    Add SortHeap.java

---
+++ java/algorithms/sort/SortHeap.java
@@ -0,0 +1,44 @@
+/**
+ * Heap Sort using looping
+ * 
+ * Time  Complexity: O(log n)
+ * Space Complexity: O(n)
+ */
+
+import java.util.*;
+
+class SortHeap {
+    public static void main(String args[]) {
+        int arr[] = { 7, 1, 5, 4, 2, 3, 6 };
+
+        System.out.print("Original: ");
+        for (int i : arr)
+            System.out.print(i + " ");
+        System.out.println();
+        
+        System.out.print("Sorted\t: ");
+        for (int i : heapSort(arr))
+            System.out.print(i + " ");
+        System.out.println();
+    }
+
+
+    // heap sort function
+    static int[] heapSort(int arr[]) {
+        // creates min heap
+        Queue<Integer> heap = new PriorityQueue<>();
+
+        // for descensing order, create max heap
+        //Queue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int i : arr)
+            heap.add(Integer.valueOf(i));
+
+        int k = 0;
+        while (!heap.isEmpty())
+            arr[k++] = heap.poll();
+
+
+        return arr;
+    }
+}
